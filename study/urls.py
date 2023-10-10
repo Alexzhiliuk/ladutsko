8d5808d47552313e467bf922b94cee8f868921cc
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2023-10-10 12:49:12 +0300

    add page with teacher list

--- study/urls.py
+++ study/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path("teachers/", views.TeachersListView.as_view(), name="teachers"),
 ]
--- study/views.py
+++ study/views.py
@@ -1,15 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse, reverse_lazy
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
 from django.http import HttpResponse
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.utils.decorators import method_decorator
+from .decorators.is_admin import admin_only
 
 
 class IndexView(LoginRequiredMixin, View):
     menu = {
         "admin": {
             "Пользователи": {
-                "Учителя": "#",
+                "Учителя": reverse_lazy("teachers"),
                 "Ученики": "#",
             },
             "Заявки": "#",
@@ -27,3 +32,18 @@ class IndexView(LoginRequiredMixin, View):
         if user.profile.type == 1:
             return render(request, "study/index.html", {"menu": self.menu["admin"]})
 
+
+@method_decorator(admin_only, name="dispatch")
+class TeachersListView(LoginRequiredMixin, ListView):
+    model = User
+    context_object_name = "objects"
+    template_name = "study/teachers.html"
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(profile__type=2)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        return context
