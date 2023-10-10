6b4a5c8ea6dc156ffb70ed7169750e5734d9278c
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2023-10-10 11:36:01 +0300

    create main page

--- study/urls.py
+++ study/urls.py
@@ -2,5 +2,5 @@ from study import views
 from django.urls import path
 
 urlpatterns = [
-
+    path("", views.IndexView.as_view(), name="index"),
 ]
--- study/views.py
+++ study/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from django.contrib import messages
 
-# Create your views here.
+
+class IndexView(LoginRequiredMixin, View):
+    menu = {
+        "admin": {
+            "Пользователи": {
+                "Учителя": "#",
+                "Ученики": "#",
+            },
+            "Заявки": "#",
+            "Группы": "#",
+            "Предметы": "#",
+            "Уроки": "#",
+            "Тесты": "#",
+            "Вопросы": "#",
+        }
+    }
+
+    def get(self, request, *args, **kwargs):
+
+        user = request.user
+        if user.profile.type == 1:
+            return render(request, "study/index.html", {"menu": self.menu["admin"]})
+
